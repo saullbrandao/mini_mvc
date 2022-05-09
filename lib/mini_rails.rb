3ad3ac2222771645dea564f269ecc3083378b42a
Saull Brandão <saull@outlook.com>
2022-05-09 09:01:12 -0300

    add controllers

--- lib/mini_rails.rb
+++ lib/mini_rails.rb
@@ -1,14 +1,31 @@
 # frozen_string_literal: true
 
 require_relative "mini_rails/version"
+require "mini_rails/routing"
 
 module MiniRails
   class Error < StandardError; end
   
   class Application
     def call(env)
-      [200, { "Content-Type" => "text/plain" },
-          ["Hello, World!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, { "Content-Type" => "text/plain" }, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
